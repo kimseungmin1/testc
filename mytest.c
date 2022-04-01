324e8d5031add2ec9cefefd463812a2d2cb86826
seungmin <72717039+kimseungmin1@users.noreply.github.com>
2022-04-01 14:09:47 +0900

    Update mytest.c

--- mytest.c
+++ mytest.c
@@ -6,8 +6,9 @@ main() {
 	int k;
 	int sum = 0;
 	float sum2 = 0;
+	float sum3 = 0;
 	
-	for(i=8;i<51;i++){
+	for(i=8;i<50;i++){
 		if(i%2 == 0){
 		
 			for(k=2;k<i;k++){
@@ -18,11 +19,10 @@ main() {
 			
 			sum2 += i/(float)sum;
 			
-		}else{
-			i++;
 		}
+		sum3 += sum2;
 	}
 	
-	printf("%.3f",sum2 );
+	printf("%.3f",sum3 );
 	
 }
